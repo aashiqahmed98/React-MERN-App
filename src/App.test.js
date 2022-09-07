59627505e37f21753903af4777c6f57e2342fb2e
Aashiq <48018540+aashiqahmed98@users.noreply.github.com>
2022-09-07 19:04:43 +0530

    added comment in test file

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+// what testing is this?
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
